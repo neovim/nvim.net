bb9de5a902073175f20c8c7638f3c1d7367839c9
b-r-o-c-k <brockmammen@gmail.com>
2018-07-10 09:37:43 -0500

    Make API generator take output path as an argument

--- src/NvimClient.APIGenerator/Program.cs
+++ src/NvimClient.APIGenerator/Program.cs
@@ -1,9 +1,27 @@
+using System;
+using System.Linq;
+using System.Reflection;
+
 namespace NvimClient.APIGenerator
 {
   internal static class Program
   {
-    public static void Main() =>
-      NvimAPIGenerator.GenerateCSharpFile(
-        "../../../../NvimClient.API/NvimAPI.generated.cs");
+    private static int Main(string[] args)
+    {
+      var outputPath = args.SingleOrDefault();
+      if (string.IsNullOrEmpty(outputPath))
+      {
+        var projectName = Assembly.GetExecutingAssembly().GetName().Name;
+        Console.WriteLine(
+          "Generates a C# class file containing wrappers for Neovim API " +
+          "functions and events.\n\n" +
+          $"Usage: dotnet run --project {projectName}.csproj output.cs\n\n" +
+          "output.cs\t\toutput path of the generated C# class file");
+        return 1;
+      }
+
+      NvimAPIGenerator.GenerateCSharpFile(outputPath);
+      return 0;
+    }
   }
 }
